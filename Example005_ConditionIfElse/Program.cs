b746344e6b82877de1dc59482ea7a4f2a98c1eb0
acid-harry <igorkrivouhov@gmail.com>
2022-09-06 01:35:05 +0300

    Example 005 Condition If Else project

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username =  Console.ReadLine();
+
+if(username.ToLower() == "acid")
+{
+    Console.WriteLine("Ура! Это же acid!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+
+}
